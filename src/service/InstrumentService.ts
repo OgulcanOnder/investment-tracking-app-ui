73121930f06b9d22104cbfe6bd02bb6969dc5698
Oğulcan Önder <canonder9246@gmail.com>
2026-07-29 11:17:25 +0300

    Update endpoint url

--- src/service/InstrumentService.ts
+++ src/service/InstrumentService.ts
@@ -1,6 +1,6 @@
 import apiClient from "../data/apiClient";
 
 export const getAllInstruments = async () => {
-  const rest = await apiClient.get("/instruments/v1");
+  const rest = await apiClient.get("/v1/instruments");
   return rest.data;
 };
--- src/service/InvestmentService.ts
+++ src/service/InvestmentService.ts
@@ -1,26 +1,26 @@
 import apiClient from "../data/apiClient";
 
 export const createInvestment = async (data: any) => {
-  const rest = await apiClient.post("/investment/v1", data);
+  const rest = await apiClient.post("/v1/investment", data);
   return rest.data;
 };
 
 export const getAllInvestment = async () => {
-  const rest = await apiClient.get("/investment/v1");
+  const rest = await apiClient.get("/v1/investment");
   return rest.data;
 };
 
 export const getAllTotalInvestment = async () => {
-  const rest = await apiClient.get("/investment/v1/totalassets");
+  const rest = await apiClient.get("/v1/investment/totalassets");
   return rest.data;
 };
 
 export const updateInvestment = async (id: number, data: any) => {
-  const rest = await apiClient.put(`/investment/v1/${id}`, data);
+  const rest = await apiClient.put(`/v1/investment/${id}`, data);
   return rest.data;
 };
 
 export const deleteInvestment = async (id: number) => {
-  const rest = await apiClient.delete(`/investment/v1/${id}`);
+  const rest = await apiClient.delete(`/v1/investment/${id}`);
   return rest;
 };
